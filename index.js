52e961499633f1a652195364bcf9714eb7223d48
mauricebutler <maurice.butler@gmail.com>
2015-02-16 16:20:54 +1000

    things

--- index.js
+++ index.js
@@ -1,44 +1,78 @@
-var JaySchema = require('jayschema'),
-    jayschema = new JaySchema(),
-    clone = require('clone'),
-    contextBaseRegex = /#\//;
+var clone = require('clone');
 
-function getBase(target, keyArray){
-    var base;
-
-    for (var i = 0; i < keyArray.length; i++) {
-        if(base){
-            base = base[keyArray[i]];
-        } else {
-            base = target[keyArray[i]];
-        }
-    }
-
-    return base;
-}
-
-function fix(data, schema, callback){
-    var result = clone(data);
-
-    jayschema.validate(data, schema, function(validationErrors){
-            var cantFix = [];
-
-            if(validationErrors){
-                for (var i = 0; i < validationErrors.length; i++) {
-                    if(validationErrors[i].constraintName === 'additionalProperties'){
-                        var contextParts = validationErrors[i].instanceContext.replace(contextBaseRegex, '').split('/');
-                        delete getBase(result, contextParts)[validationErrors[i].testedValue];
-                    } else {
-                        cantFix.push(validationErrors[i]);
+var schema = {
+        type: 'array',
+        items: [
+            {
+                type: 'string'
+            },
+            {
+                type: 'object',
+                additionalProperties: false,
+                properties: {
+                    bar: {
+                        type: 'object',
+                        additionalProperties: false,
+                        properties: {
+                            thing: {
+                                type:'string'
+                            }
+                        }
                     }
                 }
             }
+        ],
+        minItems: 2,
+        maxItems: 2
+    };
 
-            callback(cantFix.length ? cantFix : null, result);
+var data = [
+    'a string',
+    {
+        bar: {
+            thing: 'another string',
+            stuff: 'some stuff'
         }
-    );
+    }
+];
+
+function arrayValidation(data, schema){
+    var length = schema.maxItems || schema.items.length;
+
+    while(data.length > length){
+        data.pop();
+    }
+
+    for (var i = 0; i < data.length; i++) {
+        process(data[i], schema.items[i]);
+    }
 }
 
+function objectValidation(data, schema){
+    for(var key in data){
+        if(schema.additionalProperties === false && !(key in schema.properties)){
+            delete data[key];
+        } else {
+            process(data[key], schema.properties[key]);
+        }
+    }
+}
 
-module.exports = fix;
+function process(data, schema){
+    if(data.type === 'array'){
+        return arrayValidation(data, schema);
+    }
+
+    if(data.type === 'object'){
+        return objectValidation(data, schema);
+    }
+
+    return data;
+}
+
+module.exports = function(data, schema, callback){
+    var result = clone(data);
+
+    callback(null, process(result, schema));
+};
 
